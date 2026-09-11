ff1391edd778f67d64ffe722f03fca863eef8229
79dric <audriclorenzogomezcarrera@gmail.com>
2026-09-11 09:55:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-st.title("Mi aplicación :p")
+st.title("Mi aplicación :p") st.badge("#1")
 
 
 st.button("Im not a button")
+color = st.color_picker("Pick A Color", "#00f900")
+st.write("The current color is", color)
+
+
 st.balloons()
-st.badge("#1")
-
-
 st.text("hello world")
